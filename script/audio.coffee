2ba9a1154596ab9011ef2dc13772e2f82583bf4a
hoshina85 <hoshina85@gmail.com>
2012-06-05 21:44:23 +0900

    multiple audioTag

--- script/audio.coffee
+++ script/audio.coffee
@@ -1,11 +1,24 @@
 $ ->
-  $('#playBtn').click ->
+  $('#playBtn1').click ->
     $(this).toggleClass('btn-danger')
     $(this).toggleClass('btn-primary')
     $(this).children('i').toggleClass('icon-stop')
     $(this).children('i').toggleClass('icon-play')
 
-    bgm = document.getElementById('bgm')
+    bgm = document.getElementById('bgm1')
+    if bgm.paused
+      bgm.play()
+    else
+      bgm.pause()
+      bgm.currentTime = 0.0
+
+  $('#playBtn2').click ->
+    $(this).toggleClass('btn-danger')
+    $(this).toggleClass('btn-primary')
+    $(this).children('i').toggleClass('icon-stop')
+    $(this).children('i').toggleClass('icon-play')
+
+    bgm = document.getElementById('bgm2')
     if bgm.paused
       bgm.play()
     else
--- script/audio.js
+++ script/audio.js
@@ -1,13 +1,27 @@
 (function() {
 
   $(function() {
-    return $('#playBtn').click(function() {
+    $('#playBtn1').click(function() {
       var bgm;
       $(this).toggleClass('btn-danger');
       $(this).toggleClass('btn-primary');
       $(this).children('i').toggleClass('icon-stop');
       $(this).children('i').toggleClass('icon-play');
-      bgm = document.getElementById('bgm');
+      bgm = document.getElementById('bgm1');
+      if (bgm.paused) {
+        return bgm.play();
+      } else {
+        bgm.pause();
+        return bgm.currentTime = 0.0;
+      }
+    });
+    return $('#playBtn2').click(function() {
+      var bgm;
+      $(this).toggleClass('btn-danger');
+      $(this).toggleClass('btn-primary');
+      $(this).children('i').toggleClass('icon-stop');
+      $(this).children('i').toggleClass('icon-play');
+      bgm = document.getElementById('bgm2');
       if (bgm.paused) {
         return bgm.play();
       } else {
