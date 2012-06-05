d06c5a846495af1b3575b7ecb91167f9352dc9c2
hoshina85 <hoshina85@gmail.com>
2012-06-05 23:20:13 +0900

    swith audio src

--- script/audio.coffee
+++ script/audio.coffee
@@ -1,26 +1,44 @@
 $ ->
-  $('#playBtn1').click ->
-    $(this).toggleClass('btn-danger')
-    $(this).toggleClass('btn-primary')
-    $(this).children('i').toggleClass('icon-stop')
-    $(this).children('i').toggleClass('icon-play')
+  console.log 'start'
 
-    bgm = document.getElementById('bgm1')
-    if bgm.paused
-      bgm.play()
+  bgm = new Audio
+  el = ""
+  $(bgm).bind 'loadstart',(e)->
+    $(el).children('i').removeClass('icon-play').addClass('icon-refresh')
+  .bind 'loadeddata',(e)->
+    $(el).children('i').removeClass('icon-refresh').addClass('icon-play')
+    this.play()
+
+  if(bgm.canPlayType 'audio/mp3')
+    ext = '.mp3'
+  else
+    ext = '.ogg'
+
+  console.log bgm
+  console.log 'ext:'+ext
+
+  $('.play_music').click ->
+    btn = $(this)
+    el = this
+    if btn.hasClass 'btn-primary'
+      src = (btn.attr 'data-src') + ext
+      bgm.src = src
+
+      $('.play_music').each ->
+        button_close($(this))
+      button_open(btn)
     else
       bgm.pause()
-      bgm.currentTime = 0.0
+      bgm.currentTime = 0
+      button_close(btn)
 
-  $('#playBtn2').click ->
-    $(this).toggleClass('btn-danger')
-    $(this).toggleClass('btn-primary')
-    $(this).children('i').toggleClass('icon-stop')
-    $(this).children('i').toggleClass('icon-play')
 
-    bgm = document.getElementById('bgm2')
-    if bgm.paused
-      bgm.play()
-    else
-      bgm.pause()
-      bgm.currentTime = 0.0
+    #bgm = document.getElementById('bgm1')
+  
+  button_close = (btn)->
+    btn.removeClass('btn-danger').addClass('btn-primary').children('i').removeClass('icon-stop').addClass('icon-play')
+
+  button_open = (btn)->
+    btn.removeClass('btn-primary').addClass('btn-danger').children('i').removeClass('icon-play').addClass('icon-stop')
+  return
+
--- script/audio.js
+++ script/audio.js
@@ -1,34 +1,43 @@
-(function() {
 
-  $(function() {
-    $('#playBtn1').click(function() {
-      var bgm;
-      $(this).toggleClass('btn-danger');
-      $(this).toggleClass('btn-primary');
-      $(this).children('i').toggleClass('icon-stop');
-      $(this).children('i').toggleClass('icon-play');
-      bgm = document.getElementById('bgm1');
-      if (bgm.paused) {
-        return bgm.play();
-      } else {
-        bgm.pause();
-        return bgm.currentTime = 0.0;
-      }
-    });
-    return $('#playBtn2').click(function() {
-      var bgm;
-      $(this).toggleClass('btn-danger');
-      $(this).toggleClass('btn-primary');
-      $(this).children('i').toggleClass('icon-stop');
-      $(this).children('i').toggleClass('icon-play');
-      bgm = document.getElementById('bgm2');
-      if (bgm.paused) {
-        return bgm.play();
-      } else {
-        bgm.pause();
-        return bgm.currentTime = 0.0;
-      }
-    });
+$(function() {
+  var bgm, button_close, button_open, el, ext;
+  console.log('start');
+  bgm = new Audio;
+  el = "";
+  $(bgm).bind('loadstart', function(e) {
+    return $(el).children('i').removeClass('icon-play').addClass('icon-refresh');
+  }).bind('loadeddata', function(e) {
+    $(el).children('i').removeClass('icon-refresh').addClass('icon-play');
+    return this.play();
   });
-
-}).call(this);
+  if (bgm.canPlayType('audio/mp3')) {
+    ext = '.mp3';
+  } else {
+    ext = '.ogg';
+  }
+  console.log(bgm);
+  console.log('ext:' + ext);
+  $('.play_music').click(function() {
+    var btn, src;
+    btn = $(this);
+    el = this;
+    if (btn.hasClass('btn-primary')) {
+      src = (btn.attr('data-src')) + ext;
+      bgm.src = src;
+      $('.play_music').each(function() {
+        return button_close($(this));
+      });
+      return button_open(btn);
+    } else {
+      bgm.pause();
+      bgm.currentTime = 0;
+      return button_close(btn);
+    }
+  });
+  button_close = function(btn) {
+    return btn.removeClass('btn-danger').addClass('btn-primary').children('i').removeClass('icon-stop').addClass('icon-play');
+  };
+  button_open = function(btn) {
+    return btn.removeClass('btn-primary').addClass('btn-danger').children('i').removeClass('icon-play').addClass('icon-stop');
+  };
+});
